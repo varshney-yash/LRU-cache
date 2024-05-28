5c4c73bd2d3348d0e70d328f1667f767fdc0bd10
varshney-yash <107148830+varshney-yash@users.noreply.github.com>
2024-05-28 18:40:01 +0530

    clear cache and updatekey funcs

--- LRU.cpp
+++ LRU.cpp
@@ -4,8 +4,6 @@
 #include <memory>
 using namespace std;
 
-// implementing Least Recently Used cache using doubly linked list and hashmap
-
 // Store data in LL's Node
 class Node {
 public:
@@ -21,10 +19,10 @@ public:
     list<shared_ptr<Node>> l;
     unordered_map<string, list<shared_ptr<Node>>::iterator> mp;
 
-    LRUcache(int maxSize) : maxSize(maxSize >  1 ? maxSize :  1) {}
+    LRUcache(int maxSize) : maxSize(maxSize > 1 ? maxSize : 1) {}
 
     void insertKeyVal(string key, int val) {
-        if (mp.count(key) !=  0) {
+        if (mp.count(key) != 0) {
             // Replace old value and update
             auto it = mp[key];
             (*it)->value = val;
@@ -48,7 +46,7 @@ public:
     }
 
     int* getVal(string key) {
-        if (mp.count(key) !=  0) {
+        if (mp.count(key) != 0) {
             auto it = mp[key];
             l.push_front(*it);
             l.erase(it);
@@ -59,7 +57,7 @@ public:
     }
 
     bool removeKey(string key) {
-        if (mp.count(key) !=  0) {
+        if (mp.count(key) != 0) {
             auto it = mp[key];
             l.erase(it);
             mp.erase(key);
@@ -69,7 +67,7 @@ public:
     }
 
     bool containsKey(string key) {
-        return mp.count(key) !=  0;
+        return mp.count(key) != 0;
     }
 
     int getSize() {
@@ -80,23 +78,31 @@ public:
         if (!l.empty()) {
             return l.front()->key;
         }
-        return ""; 
+        return "";
+    }
+
+    void updateKey(string key, int val) {
+        if (mp.count(key) != 0) {
+            auto it = mp[key];
+            (*it)->value = val;
+        }
+    }
+
+    void clearCache() {
+        l.clear();
+        mp.clear();
     }
 };
 
-// test
 int main() {
     LRUcache lru(3);
-    lru.insertKeyVal("item1",  1);
-    lru.insertKeyVal("item2",  2);
-    lru.insertKeyVal("item3",  3);
+
+    lru.insertKeyVal("item1", 1);
+    lru.insertKeyVal("item2", 2);
+    lru.insertKeyVal("item3", 3);
 
     int* val = lru.getVal("item1");
-    if (val) {
-        cout << "Value of item1: " << *val << endl;
-    } else {
-        cout << "item1 not found in cache." << endl;
-    }
+    cout << "Value of item1: " << (val ? to_string(*val) : "not found") << endl;
 
     bool removed = lru.removeKey("item1");
     cout << "Removed item1: " << (removed ? "Yes" : "No") << endl;
@@ -108,5 +114,19 @@ int main() {
 
     cout << "Most recent key: " << lru.mostRecentKey() << endl;
 
-    return  0;
+    lru.updateKey("item2", 22);
+    val = lru.getVal("item2");
+    cout << "Updated value of item2: " << (val ? to_string(*val) : "not found") << endl;
+
+    lru.insertKeyVal("item4", 4);
+    cout << "Cache size after inserting item4: " << lru.getSize() << endl;
+    cout << "Most recent key after inserting item4: " << lru.mostRecentKey() << endl;
+    val = lru.getVal("item3");
+    cout << "Value of item3 after inserting item4: " << (val ? to_string(*val) : "not found") << endl;
+
+    lru.clearCache();
+    cout << "Cache size after clearing: " << lru.getSize() << endl;
+    cout << "Most recent key after clearing: " << lru.mostRecentKey() << endl;
+
+    return 0;
 }
